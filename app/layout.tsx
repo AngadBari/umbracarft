13ffa5f319997bdf921ad14ce8198014e8b565ff
AngadBari <angadbari2@gmail.com>
2026-05-29 19:45:20 +0530

    Layout Update

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,27 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "UmbraCraft",
   description: "Modern Box-Shadows for your next project",
+
+   openGraph: {
+    title: "UmbraCraft",
+    description: "Modern box shadows for developers.",
+    url: "https://umbracarft.vercel.app/",
+    siteName: "UmbraCraft",
+    images: [
+      {
+        url: "img/Banner.png",
+        width: 1200,
+        height: 630,
+      },
+    ],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "UmbraCraft",
+    description: "Modern box shadows for developers.",
+    images: ["img/Banner.png"],
+  },
 };
 
 export default function RootLayout({
